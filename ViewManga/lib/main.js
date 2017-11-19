1816dd0fcbe321c03d25c377f3af4c30d3d9a8ed
USER <USER@DESKTOP-LSL3HKK>
2017-11-19 15:37:23 +0530

    File Consume Path spcified

---
+++ ViewManga/lib/main.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class main {
+    constructor(valuate, parsed) {
+        this.valuate = valuate;
+        this.parsed = parsed;
+        this.appPath = valuate;
+        this.maincss = parsed;
+    }
+}
+let serverData;
+//# sourceMappingURL=main.js.map
---
+++ ViewManga/lib/main.ts
@@ -0,0 +1,16 @@
+﻿import http = require('http');
+
+class main {
+    appPath: string; //Default values to locate 
+    maincss: string;
+    constructor(public valuate: string, public parsed: string) {
+        this.appPath = valuate;
+        this.maincss = parsed;
+    }
+   
+}
+interface serverFiles {
+    serverCSS: Array<CSS>;
+    serverHTML: string;
+}
+let serverData: Array<CSS>;
